37b2ef0877068bfc8a098f6856f3a84d6a74a2e4
Youssef Khaky <youssefkhaky@hotmail.com>
2020-02-26 23:32:33 -0500

    motor speed controlling

--- MotorController/MotorController.cpp
+++ MotorController/MotorController.cpp
@@ -1,31 +1,47 @@
 #include "Arduino.h"
 #include "MotorController.h"
 
-MotorController::MotorController(int motorPinA, int motorPinB, int encoderPinA, int encoderPinB)
+MotorController::MotorController(int motorPinA, int motorPinB, int encoderPinA, int encoderPinB, int PWM_speed)
 {
     this-> motorPinA = motorPinA;
     this-> motorPinB = motorPinB;
     this-> encoderPinA = encoderPinA;
     this-> encoderPinB = encoderPinB;
+    this->PWMSpeedPid = PWMSpeedPid;
+    
 
     pinMode(this->motorPinA, OUTPUT);
     pinMode(this->motorPinB, OUTPUT);
     pinMode(this-> encoderPinA, INPUT);
     pinMode(this-> encoderPinB, INPUT);
+    pinMode(this->PWMSpeedPid, OUTPUT);
+}
+
+//
+int MotorController::RPM2PWD(int RPM){
+  if (RPM < 30 || RPM > 0){
+    return LUT[RPM];
+  } else if (RPM > 30){
+    return 255;
+  } else if (RPM < 0){
+    return 0;
+  }
 }
 
 // rotate motor forward
-void MotorController::rotateMotorForward(void)
+void MotorController::rotateMotorForward(int RPM = 30)
 {
     digitalWrite(this->motorPinA, LOW);
     digitalWrite(this->motorPinB, HIGH);
+    digitalWrite(this->PWMSpeedPid, this->RPM2PWD(RPM));
 }
 
 // rotate motor backwards
-void MotorController::rotateMotorBackwards(void)
+void MotorController::rotateMotorBackwards(int PWM_speed = 255)
 {
     digitalWrite(this->motorPinA, HIGH);
     digitalWrite(this->motorPinB, LOW);
+    digitalWrite(this->PWMSpeedPid, this->RPM2PWD(RPM));
 }
 
 // stop motor from rotating
@@ -33,25 +49,5 @@ void MotorController::stopMotorRotation(void)
 {
     digitalWrite(this->motorPinA, LOW);
     digitalWrite(this->motorPinB, LOW);
-}
-
-void MotorController::wheelSpeed()
-{
- int RFstate = digitalRead(encoderRFpinA);
- if((encoderRFPinALast == LOW) && RFstate==HIGH)
- {
-   int val = digitalRead(encoderRFpinB);
-   if(val == LOW && Direction)
-  {
-     Direction = false; //Reverse
-   }
-   else if(val == HIGH && !Direction)
-   {
-     Direction = true;  //Forward
-   }
- }
- encoderRFPinALast = RFstate;
-
- if(!Direction)  duration  ;
- else  duration--;
+    digitalWrite(this->PWMSpeedPid, 0);
 }
--- MotorController/MotorController.h
+++ MotorController/MotorController.h
@@ -1,5 +1,3 @@
-#ifndef MotorController_h
-#define MotorController_h
 #include "Arduino.h"
 class MotorController
 {
@@ -7,13 +5,15 @@ class MotorController
     int motorPinB; 
     int encoderPinA;
     int encoderPinB;
-    int encoderLBPinALast;
-    
+    int PWMSpeedPid;
+    int LUT[36] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 66, 71, 73, 78, 84, 88, 93, 97, 105, 109, 120, 128, 134, 147, 160, 171, 194, 216, 240, 249, 255, 255, 255, 255, 255, 255};
+
 public:
     MotorController(int motorPinA, int motorPinB, int encoderPinA, int encoderPinB);
     void rotateMotorForward(void);
     void rotateMotorBackwards(void);
     void stopMotorRotation(void);
+    int RPM2PWD(int RPM);
 };
 
 #endif
